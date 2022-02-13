be20d519bdcef99f861317ade457fd0c77d8b172
Arman <av3838@mail.ru>
2022-02-13 22:22:43 +0400

    conditional rendering with react router

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import {Route, useParams} from 'react-router-dom'
+import {Route, useParams, Link} from 'react-router-dom'
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
@@ -18,8 +18,15 @@ const QuoteDetail = () => {
     return (
         <>
           <HighlightedQuote text ={quote.text} author = {quote.author}/>
+          <Route path={`/quotes/${params.quoteId}`} exact>
+            <div className='centered'>
+              <Link className='btn--flat' to = {`/quotes/${params.quoteId}/comments`}>
+                Load Comments
+              </Link>
+            </div>
+          </Route>
           <Route path={`/quotes/${params.quoteId}/comments`}>
-                <Comments/>
+              <Comments/>
           </Route>
         </>
     )
