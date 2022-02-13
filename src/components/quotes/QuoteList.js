6dc90f0f6c63020f2bf1fde6144d6b5d7bcb907d
Arman <av3838@mail.ru>
2022-02-13 23:11:06 +0400

    Writing More Flexible Routing Code

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -20,14 +20,16 @@ const QuoteList = (props) => {
   const history = useHistory()
   const location = useLocation()
 
-
   const queryParams = new URLSearchParams(location.search)
   const isSortingAscending = queryParams.get('sort') === 'asc'
 
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending)
 
   const changeSortingHandler = () => {
-    history.push(`/quotes/?sort=${isSortingAscending?'desc':'asc'}`);
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending?'desc':'asc'}`
+    })
     
   }
 
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import {Route, useParams, Link} from 'react-router-dom'
+import {Route, useParams, Link, useRouteMatch} from 'react-router-dom'
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
@@ -9,23 +9,23 @@ const DUMMY_DATA =[
 
 const QuoteDetail = () => {
     const params = useParams();
+    const match = useRouteMatch()
     const quote = DUMMY_DATA.find(quote => quote.id === params.quoteId)
 
     if(!quote) {
       return <p>No quote found</p>
     }
-
     return (
         <>
           <HighlightedQuote text ={quote.text} author = {quote.author}/>
-          <Route path={`/quotes/${params.quoteId}`} exact>
+          <Route path={match.path} exact>
             <div className='centered'>
-              <Link className='btn--flat' to = {`/quotes/${params.quoteId}/comments`}>
+              <Link className='btn--flat' to = {`${match.url}/comments`}>
                 Load Comments
               </Link>
             </div>
           </Route>
-          <Route path={`/quotes/${params.quoteId}/comments`}>
+          <Route path={`${match.path}/comments`}>
               <Comments/>
           </Route>
         </>
